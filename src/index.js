51fcbab3a07df8d6c738eb15dc42b5e51bc882d8
BroDaru <92681328+BroDaru@users.noreply.github.com>
2025-12-03 11:35:43 +0000

    사이트개설

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+// import './index.css'; // (이 파일이 없으면 이 줄은 지워도 됩니다)
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-      <App />
-        </React.StrictMode>
-        );
+    <App />
+  </React.StrictMode>
+);
